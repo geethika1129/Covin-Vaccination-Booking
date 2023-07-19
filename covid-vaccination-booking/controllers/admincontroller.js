d6a43e8751c6e6a9e23b611e5383a1be8f36af82
Geethika Singareddy <geethika1129@gmail.com>
2023-07-19 12:15:37 +0530

    add

--- covid-vaccination-booking/controllers/admincontroller.js
+++ covid-vaccination-booking/controllers/admincontroller.js
@@ -1,20 +1,104 @@
 const Admin = require('../models/Admin');
+const VaccinationCentre = require('../models/Admin');
 
 const adminController = {
   login: async (req, res) => {
-    // Implementation for admin login
+    const { email, password } = req.body;
+
+    try {
+      // Check if the admin exists with the provided email
+      const admin = await Admin.findOne({ email });
+
+      if (!admin) {
+        return res.status(404).json({ error: 'Admin not found' });
+      }
+
+      // Check if the password is correct
+      if (admin.password !== password) {
+        return res.status(401).json({ error: 'Invalid password' });
+      }
+
+      // Admin login successful
+      return res.status(200).json({ message: 'Login successful', admin });
+    } catch (error) {
+      console.error('Error while logging in as admin:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   addVaccinationCentres: async (req, res) => {
-    // Implementation for adding vaccination centres
+    const { adminId, name, workingHours } = req.body;
+
+    try {
+      // Check if the admin exists
+      const admin = await Admin.findById(adminId);
+
+      if (!admin) {
+        return res.status(404).json({ error: 'Admin not found' });
+      }
+
+      // Add a new vaccination centre
+      admin.vaccinationCentres.push({ name, workingHours });
+
+      await admin.save();
+
+      return res.status(201).json({ message: 'Vaccination centre added successfully', admin });
+    } catch (error) {
+      console.error('Error while adding vaccination centre:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   getDosageDetails: async (req, res) => {
-    // Implementation for getting dosage details grouped by centres
+    try {
+      // Group dosage details by vaccination centres
+      const dosageDetails = await User.aggregate([
+        { $unwind: '$vaccinationSlots' },
+        {
+          $group: {
+            _id: '$vaccinationSlots.vaccinationCentre',
+            centreName: { $first: '$vaccinationSlots.vaccinationCentre' },
+            totalDosages: { $sum: 1 }
+          }
+        },
+        { $project: { _id: 0, centreName: 1, totalDosages: 1 } }
+      ]);
+
+      return res.status(200).json({ dosageDetails });
+    } catch (error) {
+      console.error('Error while getting dosage details:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   removeVaccinationCentre: async (req, res) => {
-    // Implementation for removing vaccination centre
+    const { adminId, centreId } = req.body;
+
+    try {
+      // Check if the admin exists
+      const admin = await Admin.findById(adminId);
+
+      if (!admin) {
+        return res.status(404).json({ error: 'Admin not found' });
+      }
+
+      // Find the index of the vaccination centre in the admin's list
+      const centreIndex = admin.vaccinationCentres.findIndex((centre) => centre.id === centreId);
+
+      if (centreIndex === -1) {
+        return res.status(404).json({ error: 'Vaccination centre not found' });
+      }
+
+      // Remove the vaccination centre
+      admin.vaccinationCentres.splice(centreIndex, 1);
+
+      await admin.save();
+
+      return res.status(200).json({ message: 'Vaccination centre removed successfully', admin });
+    } catch (error) {
+      console.error('Error while removing vaccination centre:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   }
 };
 
--- covid-vaccination-booking/controllers/usercontroller.js
+++ covid-vaccination-booking/controllers/usercontroller.js
@@ -2,23 +2,106 @@ const User = require('../models/User');
 
 const userController = {
   login: async (req, res) => {
-    // Implementation for user login
+    const { email, password } = req.body;
+
+    try {
+      // Check if the user exists with the provided email
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // Check if the password is correct
+      if (user.password !== password) {
+        return res.status(401).json({ error: 'Invalid password' });
+      }
+
+      // User login successful
+      return res.status(200).json({ message: 'Login successful', user });
+    } catch (error) {
+      console.error('Error while logging in:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   signup: async (req, res) => {
-    // Implementation for user signup
+    const { name, email, password, role } = req.body;
+
+    try {
+      // Check if the email is already registered
+      const existingUser = await User.findOne({ email });
+
+      if (existingUser) {
+        return res.status(409).json({ error: 'Email already registered' });
+      }
+
+      // Create a new user
+      const newUser = new User({ name, email, password, role });
+
+      // Save the user to the database
+      await newUser.save();
+
+      return res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+      console.error('Error while signing up:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   searchVaccinationCentres: async (req, res) => {
-    // Implementation for searching vaccination centres
+    try {
+      // Assuming you have a model for VaccinationCentre with fields: name and workingHours
+      const vaccinationCentres = await VaccinationCentre.find({});
+
+      return res.status(200).json({ vaccinationCentres });
+    } catch (error) {
+      console.error('Error while searching vaccination centres:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   applyForVaccinationSlot: async (req, res) => {
-    // Implementation for applying for a vaccination slot
+    try {
+      const { userId, vaccinationCentreId, slotDate } = req.body;
+
+      // Check if the user exists
+      const user = await User.findById(userId);
+
+      if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+
+      // Check if the user already has 10 vaccination slots for the day
+      if (user.vaccinationSlots.length >= 10) {
+        return res.status(403).json({ error: 'Maximum slots reached for the day' });
+      }
+
+      // Apply for the vaccination slot
+      user.vaccinationSlots.push({
+        vaccinationCentre: vaccinationCentreId,
+        slotDate: new Date(slotDate)
+      });
+
+      await user.save();
+
+      return res.status(200).json({ message: 'Vaccination slot applied successfully', user });
+    } catch (error) {
+      console.error('Error while applying for a vaccination slot:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   },
 
   logout: async (req, res) => {
-    // Implementation for user logout
+    try {
+      // Assuming you have a session mechanism, you can clear the user session here
+      // For example, if you're using express-session:
+      req.session.destroy();
+      res.status(200).json({ message: 'Logout successful' });
+    } catch (error) {
+      console.error('Error while logging out:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
   }
 };
 
