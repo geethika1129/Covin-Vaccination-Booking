0f4a67055101974fe4cf771f0ab37fd37aca8182
Geethika Singareddy <geethika1129@gmail.com>
2023-07-20 16:58:07 +0530

    add

--- covid-vaccination-booking/routes/userroutes.js
+++ covid-vaccination-booking/routes/userroutes.js
@@ -5,8 +5,8 @@ const userController = require('../controllers/usercontroller');
 // User Routes
 router.post('/login', userController.login);
 router.post('/signup', userController.signup);
-router.get('/vaccination-centres', userController.searchVaccinationCentres);
-router.post('/book', userController.applyForVaccinationSlot);
+router.get('/searchCentre', userController.searchCentre);
+router.post('/bookAppointment', userController.applyForVaccinationSlot);
 router.post('/logout', userController.logout);
 
 module.exports = router;
